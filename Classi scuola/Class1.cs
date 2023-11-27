63ff4146902581ecf38a0501ae093780ae49c921
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2023-11-27 16:00:24 +0100

    Aggiungere i file di progetto.

---
+++ Classi scuola/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi_scuola
+{
+    public class Persona
+
+    {
+        public int MyProprerty { get; set; }
+        public string Nome;
+        public string Cognome;
+        public DateTime Datan;
+        public DateTime Datav;
+        public float voto;
+        
+
+    }
+}
---
+++ Classi scuola/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi_scuola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Studente = PersonaStudente();
+            Persona Insegnante = PersonaInsegante();
+        }
+
+        private static Persona PersonaInsegante()
+        {
+            Persona s = new Persona();
+            Console.WriteLine("nome: ");
+            s.Nome = Console.ReadLine();
+            Console.WriteLine("cognome: ");
+            s.Cognome = Console.ReadLine();
+            Console.WriteLine("Data di nascita: ");
+            s.Datan = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("voto: ");
+            s.voto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Data del voto");
+            s.Datav = DateTime.Parse(Console.ReadLine());
+            return s;
+
+        }
+
+        private static Persona PersonaStudente()
+        {
+            Persona s = new Persona();
+            Console.WriteLine("nome: ");
+            s.Nome = Console.ReadLine();
+            Console.WriteLine("cognome: ");
+            s.Cognome = Console.ReadLine();
+            Console.WriteLine("Data di nascita: ");
+            s.Datan = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("voto: ");
+            s.voto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Data del voto");
+            s.Datav = DateTime.Parse(Console.ReadLine());
+            return s;
+        }
+    }
+}
